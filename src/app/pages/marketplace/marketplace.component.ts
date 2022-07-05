82dfffbb24c271d7b542de375830d43543c128c8
Junior Rojas <rojasjuniore@gmail.com>
2022-07-05 18:47:18 -0500

    stratutura de markeplace

--- src/app/pages/marketplace/marketplace.component.ts
+++ src/app/pages/marketplace/marketplace.component.ts
@@ -7,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarketplaceComponent implements OnInit {
 
+
+  listCard = [{
+    title: 'Card 1',
+    description: 'This is a description',
+    by: 'This is a bio',
+    image1: './assets/img/collection.jpg',
+    image2: './assets/img/collection.jpg',
+    smartContract: '0x0',
+  }, {
+    title: 'Card 2',
+    description: 'This is a description',
+    by: 'This is a bio',
+    image1: './assets/img/collection.jpg',
+    image2: './assets/img/collection.jpg',
+    smartContract: '0x0',
+  }, {
+    title: 'Card 3',
+    description: 'This is a description',
+    by: 'This is a bio',
+    image1: './assets/img/collection.jpg',
+    image2: './assets/img/collection.jpg',
+    smartContract: '0x0',
+  }]
+
   constructor() { }
 
   ngOnInit(): void {
