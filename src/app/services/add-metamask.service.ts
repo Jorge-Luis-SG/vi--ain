3b63368eac14a0643f041b5c129b785f2385f133
Junior Rojas <rojasjuniore@gmail.com>
2022-10-07 18:19:27 -0500

    migracion

--- src/app/services/add-metamask.service.ts
+++ src/app/services/add-metamask.service.ts
@@ -1,46 +1,112 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { Sweetalert2Service } from './sweetalert2.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddTokenAMetamaskService {
 
-  token: any
   constructor() { }
 
   async addToken() {
     try {
 
-      let data1: any = localStorage.getItem('_data_contract')
-      this.token = JSON.parse(data1);
-      console.log("this.token ", this.token)
-
       // wasAdded is a boolean. Like any RPC method, an error may be thrown.
-      
       const wasAdded = await window.ethereum.request({
         method: 'wallet_watchAsset',
         params: {
           type: 'ERC20', // Initially only supports ERC20, but eventually more!
           options: {
-            address: this.token.contractAddress, // The address that the token is at.
-            symbol: this.token.symbol, // A ticker symbol or shorthand, up to 5 chars.
-            decimals: this.token.decimals, // The number of decimals in the token
+            address: environment.mainToken.contract, // The address that the token is at.
+            symbol: environment.mainToken.symbol, // A ticker symbol or shorthand, up to 5 chars.
+            decimals: environment.mainToken.decimals, // The number of decimals in the token
             image: environment.urlTokenLogo, // A string url of the token logo
           },
         },
       });
 
-      if (wasAdded) {
-        console.log('Thanks!');
-        alert('Thanks!');
-      } else {
-        console.log('Your loss!');
-        alert('Your loss!');
-      }
+      // console.log({wasAdded});
+
+      // if (!wasAdded) { return; }
+
+      // return await this.sweetAlertSrv.showSuccess(`${environment.mainToken.symbol} was added`);
+
     } catch (error) {
-      alert(JSON.stringify(error));
+      // alert(JSON.stringify(error));
       console.log(error);
+    }
+  }
+
+
+  /**
+   * Registrar cadena en la billetera
+   * @returns 
+   */
+  async addChainId(){
+    try {
+      const wasAdded = await window.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [
+          {
+            chainId: environment.chain.chainIdMetamask,
+            rpcUrls: environment.chain.rpcUrls,
+            chainName: environment.chain.chainName,
+            nativeCurrency: environment.chain.nativeCurrency,
+            blockExplorerUrls: environment.chain.blockExplorerUrls,
+          }
+        ],
+      });
+
+      // console.log({wasAdded});
+      return wasAdded;
+    } catch (err: any) {
+
+      /** Si tiene trasacciones pendientes por ejecutar */
+      if(err.code === 4001){ 
+        err.message = 'Please, confirm the request for change of network';
+      }
+
+      /** Si tiene trasacciones pendientes por ejecutar */
+      if(err.code === -32002){ 
+        err.message = 'You have pending requests on your wallet. Please, check on your wallet before continuing';
+      }
+
+      throw err;
+    }
+  }
+
+
+  /**
+   * Disparar método para cambiar de red o añadirla
+   * @returns 
+   */
+  async changeChainIdOrAdd(){
+    try {
+      const tryChange = await window.ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: environment.chain.chainIdMetamask }],
+      });
+
+      // console.log({tryChange});
+
+      return tryChange
+    } catch (err: any) {
+
+      /** Si no tiene la red registrada */
+      if(err.code === 4902){ return await this.addChainId(); }
+
+      /** Si tiene trasacciones pendientes por ejecutar */
+      if(err.code === 4001){ 
+        err.message = 'Please, confirm the request for change of network';
+      }
+
+      /** Si tiene trasacciones pendientes por ejecutar */
+      if(err.code === -32002){ 
+        err.message = 'You have pending requests on your wallet. Please, check on your wallet before continuing';
+      }
+
+      throw err;
     }
   }
 
@@ -50,9 +116,11 @@ export class AddTokenAMetamaskService {
         method: 'wallet_switchEthereumChain',
         params: [{ chainId: environment.chain.chainIdMetamask }],
       });
-    } catch (error: any) {
-      console.log("error", error)
-      if (error.code === 4902) {
+      return true;
+    } catch (err: any) {
+      console.log("error", err)
+
+      if (err.code === 4902) {
         try {
           const wasAdded = await window.ethereum.request({
             method: 'wallet_addEthereumChain',
@@ -67,18 +135,25 @@ export class AddTokenAMetamaskService {
             ],
           });
 
+
           if (wasAdded) {
-            console.log('Thanks!');
-            alert('Thanks!');
+            // console.log('Thanks!');
+            // alert('Thanks!');
+            return true;
           } else {
-            console.log('Your loss!');
-            alert('Your loss!');
+            // console.log('Your loss!');
+            // alert('Your loss!');
+            return false;
           }
         } catch (error: any) {
+          throw error;
           console.log("error", error)
-          alert(JSON.stringify(error));
+          // alert(JSON.stringify(error));
+          return false;
         }
       }
+
+      return false;
     }
   }
 }
